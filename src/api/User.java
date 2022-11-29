414d20aad7b01354b1cbcd1032febb8c1b5c4c32
Dimos Theocharis <dimostheocharis10@gmail.com>
2022-11-29 23:22:01 +0200

    Initialized the class User.

---
+++ src/api/User.java
@@ -0,0 +1,38 @@
+package api;
+
+public class User {
+    private String name;
+    private String surname;
+    private String username;
+    private int password;
+    private String type;
+
+    public User(String name, String surname, String username, int password, String type) {
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.password = password;
+        this.type = type;
+    }
+
+    //Getters
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public int getPassword() {
+        return this.password;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+}
