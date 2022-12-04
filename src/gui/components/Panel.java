ff2008bb288c87a2d86f9ad5efe26b675c0bf5a2
Dimos Theocharis <dimostheocharis10@gmail.com>
2022-12-05 00:24:43 +0200

    Initialized the SignTemplate class. Bug with centering.

--- src/gui/components/Panel.java
+++ src/gui/components/Panel.java
@@ -1,10 +1,21 @@
 package gui.components;
 
+
 import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Component;
 
 public class Panel extends JPanel {
-    public Panel() {
+    public Panel(int width, int height, Color backgroundColor) {
+        this.setSize(width, height);
+        this.setBackground(backgroundColor);
+        this.setOpaque(true);
+    }
 
+    public void center(JFrame frame) {
+        this.setLocation(frame.getWidth() / 2 - this.getWidth() / 2, 0);
+        this.setLocation((frame.getWidth() - this.getWidth()) / 2, (frame.getHeight() - this.getHeight()) / 2);
     }
 
 }
--- src/gui/screens/SignTemplate.java
+++ src/gui/screens/SignTemplate.java
@@ -1,4 +1,41 @@
 package gui.screens;
 
-public class SignTemplate {
+import gui.bootstrap.Colors;
+import gui.components.Panel;
+
+import javax.swing.JFrame;
+import java.awt.*;
+
+//This class is a template that will be used by the ui classes SignUpScreen & SignInScreen
+//It contains code that both these classes have in common
+//It creates a frame 1080x720, centers a panel where the content will be displayed, and applies styles
+
+public class SignTemplate extends JFrame {
+    private int width = 1080;
+    private int height = 720;
+
+    Panel container;
+
+    public SignTemplate() {
+        //initialization
+        this.setSize(this.width, this.height);
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.container = new Panel(this.width / 2, this.height / 2, Colors.secondaryColor);
+        this.container.center(this);
+
+        //styling
+        this.setSize(this.width, this.height);
+        this.getContentPane().setBackground(Colors.primaryColor);
+
+        //additions
+        this.add(this.container);
+
+        this.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        SignTemplate p = new SignTemplate();
+    }
 }
