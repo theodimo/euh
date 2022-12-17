efc039585f6c8a649a0b978b32f6172d9b967198
Dimos Theocharis <dimostheocharis10@gmail.com>
2022-12-17 21:14:14 +0200

    Added extra colors and fonts.

--- src/gui/bootstrap/Colors.java
+++ src/gui/bootstrap/Colors.java
@@ -5,5 +5,10 @@ import java.awt.Color;
 public class Colors {
     public static Color primaryColor = new Color(237,247,255);
     public static Color secondaryColor = new Color(21,47,80);
+
     public static Color accentColor = new Color(166,207,213);
+
+    public static Color paleBlue = new Color(29, 75, 135);
+
+    public static Color white = new Color(255, 255, 255);
 }
--- src/gui/bootstrap/Fonts.java
+++ src/gui/bootstrap/Fonts.java
@@ -3,7 +3,8 @@ package gui.bootstrap;
 import java.awt.Font;
 
 public class Fonts {
-    public static Font main = new Font("Rapor", Font.PLAIN, 16);
+    public static Font mainFont = new Font("Rapor", Font.PLAIN, 16);
     public static Font inputLabel = new Font("Rapor", Font.PLAIN, 18);
     public static Font titleFont = new Font("Rapor", Font.PLAIN, 30);
+    public static Font smallFont = new Font("Rapor", Font.PLAIN, 14);
 }
